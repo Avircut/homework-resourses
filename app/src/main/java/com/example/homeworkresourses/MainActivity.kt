f46087e92d8e6cf894374b4c9f15394431376908
Avircut <lomoff.slaw@yandex.ru>
2020-02-05 01:26:45 +0700

    initial commit

--- app/src/main/java/com/example/homeworkresourses/MainActivity.kt
+++ app/src/main/java/com/example/homeworkresourses/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.homeworkresourses
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text="""
+            Flavor=${BuildConfig.FLAVOR}
+            BuildType=${BuildConfig.BUILD_TYPE}
+            VersionCode=${BuildConfig.VERSION_CODE}
+            VersionName=${BuildConfig.VERSION_NAME}
+            AppID=${BuildConfig.APPLICATION_ID}
+        """.trimIndent()
     }
 }
